02618aea7fed263294120674d81942edc561d25f
Agriffanuzraa <agriffaanzr@gmail.com>
2025-02-27 11:39:37 +0700

    2.0

--- meet2.php
+++ meet2.php
@@ -3,92 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculator by Agriffa</title>
+    <title>Document</title>
     <style>
-        .form{
+        p{
             color: black;
+            font-weight: 900;
         }
-        .container{
-            margin: auto;
-            margin-top: 5rem;
-            color: black;
-            text-align: center;
-            width: 200px;
-            height: 200px;
-            background-image: linear-gradient(90deg, rgba(104,249,232,1) 0%, rgba(118,9,121,1) 100%, rgba(0,212,255,1) 100%);
-            padding: 2rem;
-        }
-        button{
-            margin-top: 2rem;
+        .login{
             background-color: pink;
-            font-weight: 600;
-        }
-        button:hover{
-            color: blueviolet;
-            background-color: white;
+            border: 1px solid whitesmoke;
+            width: 400px;
+            padding: 2rem;
+            text-align: center;
+            margin: auto;
+            margin-top: 10rem;
         }
         input{
-            border-radius: 5px;
+            border-radius: 10px;
             text-align: center;
+            padding: 10px;
+        }
+        .button{
+            margin-top: 2rem;
+            border-radius: 5px;
+            background-color: lightsalmon;
+            width: 4rem;
+        }
+        .button:hover{
+            background-color: white;
+        }
+        .hi{
+            color:purple;
         }
     </style>
 </head>
 <body>
-    <div class="container">
-        <p>Calculator</p>
-        <form class="form" method="post">
-            <label>first numbers: </label>
-            <input type="number" class="number" name="angka1" placeholder="input first number">
-            <br>
-            <label>second numbers: </label>
-            <input type="number" class="number" name="angka2" placeholder="input second number"><br>
-            <button name="button" value="+">+</button>
-            <button name="button" value="-">-</button>
-            <button name="button" value="/">/</button>
-            <button name="button" value="*">x</button>
-        </form>
-        <?php
-            // echo "hi";
-            if(isset($_POST["button"])){
-                $setButton = $_POST["button"];
-                $angka1 = (int)$_POST["angka1"];
-                $angka2 = (int)$_POST["angka2"];
-                if ($setButton== "+"){
-                    echo $angka1 + $angka2;
-                }
-                elseif ($setButton== "-"){
-                    echo $angka1 - $angka2;
-                }
-                elseif ($setButton== "/"){
-                    echo $angka1 / $angka2;
-                }
-                elseif ($setButton== "*"){
-                    echo $angka1 * $angka2;
-                }
-            }
-        ?>
-    </div>
-
+    <form method="POST" class="login">
+        <p>Username</p>
+        <input name="name" placeholder="masukan username anda">
+        <p>Password</p>
+        <input name="Pass" placeholder="masukan password anda" required>
+        <br>
+        <button name="button" class="button">Login</button>
+        <p><?php
+// kalo get ga private, kalo post private
+    if (isset($_POST['button'])){
+        $nama= $_POST['name'];
+        $Pass= $_POST['Pass'];
+        $username = "Agriffa";
+        $password = "codingbee123";
+        if ($nama == $username && $Pass == $password){
+            echo "Hallo! $nama,<br>Login Succefully! <br>Kamu harus semangat ya hari ini😊";
+        } else{
+            echo"<p class='hi'>oh no username or password you wrong!</p>";
+        }
+         // Ganti "index.php" dengan nama file
+    exit();
+    }
+?> </p>
+    </form>
 </body>
-<!-- // if(isset($_POST["plus"])){
-//     $angka1 = (int)$_POST["angka1"];
-//     $angka2 = (int)$_POST["angka2"];
-//     echo $angka1 + $angka2;
-// }
-// elseif(isset($_POST["minus"])){
-//     $angka1 = (int)$_POST["angka1"];
-//     $angka2 = (int)$_POST["angka2"];
-//     echo $angka1 - $angka2;
-// }
-// elseif(isset($_POST["devide"])){
-//     $angka1 = (int)$_POST["angka1"];
-//     $angka2 = (int)$_POST["angka2"];
-//     echo $angka1 / $angka2;
-// }
-// elseif(isset($_POST["multiplication"])){
-//     $angka1 = (int)$_POST["angka1"];
-//     $angka2 = (int)$_POST["angka2"];
-//     echo $angka1 * $angka2;
-// } -->
 
 </html>
